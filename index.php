b1b7725d7b9aefa94facc29bdd34ab9ef9705e21
aditya s <adityashridharani@fas.harvard.edu>
2013-10-29 17:25:25 +0530

    Added project1 index file for third/fourth time

--- index.php
+++ index.php
@@ -1,8 +1,41 @@
+<!DOCTYPE html>
+
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Welcome to P1</p>'; ?> 
- </body>
+	<body>
+
+		<head>
+			<style>
+
+				body
+					{
+					background-color:lightblue;
+					}
+
+				h3
+					{
+					color:Maroon;
+					text-align:center;
+					}
+
+				p
+					{
+					font-family:"Times New Roman";
+					font-size:20px;
+					}
+			</style>
+		</head>
+
+
+						<h3>Welcome to Project 1 - DWA</h3>
+
+							<p>Name: Aditya Shridharani</p>
+
+							<p>Bio: I am an Engineering Graduate from India. I am tuning in online
+							to sharpen my computer skills. In my free time I like to listen to music</p>
+
+							<p>I am using 'Sublime Text 2'</p>
+
+
+
+	</body>
 </html>
